40f760972996154e286d9a250fec82c7c38ae777
PSEH-5 <vsharma118@sapient.com>
2019-07-13 11:52:03 +0530

    skelten project

---
+++ src/main/java/com/sapient/footballasses/FootballassesApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.footballasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballassesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sapient/footballasses/controller/FootBallAssesmentController.java
@@ -0,0 +1,14 @@
+package com.sapient.footballasses.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class FootBallAssesmentController {
+	@GetMapping("/welcome")
+	public String getIndex() {
+		return "welcome to Sapient Assesment Test";
+	}
+}
